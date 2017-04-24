64844fcccba257bb945b284a4dd52aa55c9ad13e
Jordan Mysliwiec and Nick Wise <jordan+nick@>
2017-04-24 11:47:20 -0700

    add initial directories for css, js, img.

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1,number2));
+
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(sub(number1,number2));
+
+
+var div = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(div(number1,number2));
+
+var multi = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(multi(number1,number2));
