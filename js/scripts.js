5f5ee1c5595a8a0ffab08299d175fce73e88eb1a
Jordan Mysliwiec and Nick Wise <jordan+nick@>
2017-04-24 13:14:01 -0700

    add .js containing simple business logic for a calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 var add = function(number1, number2) {
   return number1 + number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("Enter a number to add to:"));
+var number2 = parseInt(prompt("Enter a number to add:"));
 alert(add(number1,number2));
 
 var sub = function(number1, number2) {
   return number1 - number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("Enter a number to subtract from:"));
+var number2 = parseInt(prompt("Enter a number to subtract:"));
 alert(sub(number1,number2));
 
 
@@ -17,14 +17,14 @@ var div = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("Enter a number to divide into:"));
+var number2 = parseInt(prompt("Enter a number to divide by:"));
 alert(div(number1,number2));
 
 var multi = function(number1, number2) {
   return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("Enter a number to multiply:"));
+var number2 = parseInt(prompt("Enter a number to multiply against:"));
 alert(multi(number1,number2));
